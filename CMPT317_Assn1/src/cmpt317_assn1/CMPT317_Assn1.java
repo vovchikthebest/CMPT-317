62f38df526398f82904264e11713047cf9bf0b25
jam943 <jam943@mail.usask.ca>
2016-01-29 22:09:35 -0600

    NetBeans project created
    Classes created:
    SearchStack
    StackNode
    DepthFirstSearch
    GraphNode - For Vlad to use as the graph nodes. Very little added to
    this class. Basically the visited variable and associated methods with
    it thought of so far.

---
+++ CMPT317_Assn1/src/cmpt317_assn1/CMPT317_Assn1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmpt317_assn1;
+
+/**
+ *
+ * @author jared
+ */
+public class CMPT317_Assn1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Main class for assignment 1. 
+    }
+    
+}
