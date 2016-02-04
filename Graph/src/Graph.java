8915ae38dd182f9bf1e2f8a56caec950173f51a3
vsg235 <vsg235@mail.usask.ca>
2016-02-04 10:02:20 -0600

    Completed graph construction and added addEdge, getNeighboors

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,14 +1,30 @@
 import java.util.ArrayList;
 
 public class Graph {
-	Node[] graphNodes;
-	int numNodes;
-	ArrayList<Node>[] nodeConnections;
+	Node[][] graphNodes;
+	int nodesPerSide;
+	ArrayList<Node>[][] nodeConnections;
 	
+	/**
+	 * Creates an inNumNodes * inNumNodes graph
+	 * @param inNumNodes length of graph side
+	 */
 	public Graph (int inNumNodes) {
-		numNodes = inNumNodes;
-		graphNodes = new Node[numNodes];
-		nodeConnections = new ArrayList[numNodes];
+		nodesPerSide = inNumNodes;
+		graphNodes = new Node[nodesPerSide][nodesPerSide];
+		for (int i = 0; i < nodesPerSide; i++) {
+			for (int j = 0; j < nodesPerSide; j++) {
+				graphNodes[i][j] = new Node(i, j);
+			}
+		}
+		nodeConnections = new ArrayList[nodesPerSide][nodesPerSide];
 	}
 	
+	public boolean addEdge (Node v1, Node v2) {
+		return false;
+	}
+	
+	public Node[] getNeighboors (Node v1) {
+		return null;
+	}
 }
