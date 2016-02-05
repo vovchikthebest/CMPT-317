351a63f01ea1b0ff5fc5fee4c46c8831b576ee16
vsg235 <vsg235@mail.usask.ca>
2016-02-05 13:11:37 -0600

    Added Edge class and constructor for edge class

---
+++ CMPT317_Assn1/src/cmpt317_assn1/Edge.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmpt317_assn1;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class Edge {
+    Node destination;
+    int weight;
+    
+    public Edge (Node inDes) {
+        destination = inDes;
+        weight = 1;
+    }
+    
+    public Edge (Node inDes, int inWeight) {
+        destination = inDes;
+        weight = inWeight;
+    }
+}
