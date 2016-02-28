1cc08628aadf69eacb08f1ab99775d13dc3cc009
vsg235 <vsg235@mail.usask.ca>
2016-02-28 12:29:33 -0600

    Added Assignment 2 Project to the repository

--- CMPT317_Assn1/src/cmpt317_assn1/CMPT317_Assn1.java
+++ CMPT317_Assn1/src/cmpt317_assn1/CMPT317_Assn1.java
@@ -5,9 +5,7 @@
  */
 package cmpt317_assn1;
 
-import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Stack;
 
 /**
@@ -20,90 +18,102 @@ public class CMPT317_Assn1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int size = 5;
-        int numPackages = 3;
-        int courierCapacity = 1;
+        int size = 100;
+        int numPackages = 10000;
+        int courierCapacity = 5;
         int numCouriers = 1;
-        
+
         Graph testGraph = new Graph(size);
-        
+
         testGraph.constructCityGraph();
-        
+
         // Randomize Courier Position
-        int courierPos = randInt(0,(size*size)-1);
-        Courier testCourier = new Courier (testGraph.graphNodes[courierPos], courierCapacity);
-        
+        int courierPos = randInt(0, (size * size) - 1);
+        Courier testCourier = new Courier(testGraph.graphNodes[courierPos], courierCapacity);
+
         // Randomize Package Position
         ArrayList<Package> packages = new ArrayList<Package>(numPackages);
         Package testPackage = null;
         for (int i = 0; i < numPackages; i++) {
-            int randomStart = randInt(0, (size*size)-1);
-            int randomFinish = randInt(0, (size*size)-1);
-            testPackage = new Package (testGraph.graphNodes[randomStart], testGraph.graphNodes[randomFinish]);
+            int randomStart = randInt(0, (size * size) - 1);
+            int randomFinish = randInt(0, (size * size) - 1);
+            testPackage = new Package(testGraph.graphNodes[randomStart], testGraph.graphNodes[randomFinish]);
             packages.add(testPackage);
         }
-        
+
         // DFS Search with single package single carrier
         long startTime = System.nanoTime();
         Stack<Node> resultNodes = Search.DepthFirst(testCourier.startPos, testPackage.currentNode, testGraph);
         long endTime = System.nanoTime();
-        
-        long duration = (endTime - startTime)/1000000;
-        
+
+        long duration = (endTime - startTime) / 1000000;
+
         System.out.println("--- DFS search ---");
-        System.out.println("Path: " + resultNodes);
+        if (size < 20) {
+            System.out.println("Path: " + resultNodes);
+        }
         System.out.println("Time (ms): " + String.valueOf(duration));
-        
+
         ArrayList<Package> holder = new ArrayList<Package>();
         holder.add(testPackage);
-        DrawGraph dfsDraw = new DrawGraph(testGraph, resultNodes, testCourier, holder , "DFS Draw");
-        
-        dfsDraw.setVisible(true);
-        dfsDraw.setSize(400,400);
-        dfsDraw.paintAgain();
-        
+        if (size < 20) {
+            DrawGraph dfsDraw = new DrawGraph(testGraph, resultNodes, testCourier, holder, "DFS Draw");
+
+            dfsDraw.setVisible(true);
+            dfsDraw.setSize(400, 400);
+            dfsDraw.paintAgain();
+        }
+
         // A* Search with sinple package single carrier
         startTime = System.nanoTime();
         resultNodes = Search.CompleteSearch(testCourier, testPackage, testGraph);
         endTime = System.nanoTime();
-        
-        duration = (endTime - startTime)/1000000;
-        
+
+        duration = (endTime - startTime) / 1000000;
+
         System.out.println("--- A* Search with sinple package single carrier ---");
-        System.out.println("Path: " + resultNodes);
+        if (size < 20) {
+            System.out.println("Path: " + resultNodes);
+        }
         System.out.println("Time (ms): " + String.valueOf(duration));
-        
-        DrawGraph aStarDraw = new DrawGraph(testGraph, resultNodes, testCourier, holder, "A* Single Package");
-        
-        aStarDraw.setVisible(true);
-        aStarDraw.setSize(400,400);
-        aStarDraw.paintAgain();
-        
+
+        if (size < 20) {
+            DrawGraph aStarDraw = new DrawGraph(testGraph, resultNodes, testCourier, holder, "A* Single Package");
+
+            aStarDraw.setVisible(true);
+            aStarDraw.setSize(400, 400);
+            aStarDraw.paintAgain();
+        }
+
         // A* Search with multiple packages
         holder = new ArrayList(packages);
-        
+
         startTime = System.nanoTime();
         resultNodes = Search.CompleteSearch(testCourier, packages, testGraph);
         endTime = System.nanoTime();
-        
-        duration = (endTime - startTime)/1000000;
+
+        duration = (endTime - startTime) / 1000000;
 
         System.out.println("--- A* search with multiple packages single carrier ---");
-        System.out.println("Path: " + resultNodes);
+        if (size < 20) {
+            System.out.println("Path: " + resultNodes);
+        }
         System.out.println("Time (ms): " + String.valueOf(duration));
-        
-        DrawGraph aStarDraw2 = new DrawGraph(testGraph, resultNodes, testCourier, holder, "A* Multiple Package");
-        
-        aStarDraw2.setVisible(true);
-        aStarDraw2.setSize(400,400);
-        aStarDraw2.paintAgain();
+
+        if (size < 20) {
+            DrawGraph aStarDraw2 = new DrawGraph(testGraph, resultNodes, testCourier, holder, "A* Multiple Package");
+
+            aStarDraw2.setVisible(true);
+            aStarDraw2.setSize(400, 400);
+            aStarDraw2.paintAgain();
+        }
 
         System.out.println("BLUE = Courier Start, RED = Package Start, GREEN = Package Dest");
         // NEED FOR SPEED
     }
-    
-    public static int randInt (int min, int max) {
-        return (min + (int)(Math.random() * ((max - min) + 1)));
+
+    public static int randInt(int min, int max) {
+        return (min + (int) (Math.random() * ((max - min) + 1)));
     }
-    
+
 }
---
+++ Cmpt317_Assn2/src/cmpt317_assn2/Cmpt317_Assn2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cmpt317_assn2;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class Cmpt317_Assn2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
