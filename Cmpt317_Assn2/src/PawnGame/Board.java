227914ed62a6de729ae38994a4dccf0b413ec3d4
vsg235 <vsg235@mail.usask.ca>
2016-03-03 21:15:55 -0600

    Added a function to determine a winner from the board

--- Cmpt317_Assn2/src/PawnGame/Board.java
+++ Cmpt317_Assn2/src/PawnGame/Board.java
@@ -29,4 +29,44 @@ public class Board {
         
         return temp;
     }
+    
+    /**
+     * Determine if there is a winner on the current board
+     * @return 1 - white wins, -1 - black wins, 0 - none/tie
+     */
+    public int determineWinner() {
+        int countWhite = 0, countBlack = 0;
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                // Count the white and black pieces to determine winner
+                switch(this.gameBoard[i][j]) {
+                    case(1):
+                        countWhite++;
+                        break;
+                    case(2):
+                        countBlack++;
+                        break;
+                }
+                
+                // Check if any of the white/black pieces have reached their endzones
+                if (i == 0) {
+                    if (this.gameBoard[i][j] == 2) {    // if a black piece reached endzone
+                        return -1;
+                    }
+                } else if (i == this.size - 1) {
+                    if (this.gameBoard[i][j] == 1) {    // if a white piece reached endzone
+                        return 1;
+                    }
+                }
+            }
+        }
+        
+        if (countWhite == 0) {
+            return -1;  // Black wins
+        } else if (countBlack == 0) {
+            return 1;   // White wins
+        }
+        
+        return 0;
+    }
 }
